58cde3bf017bfeb4807ef700fa688169bde62623
Tonet21 <143603441+Tonet21@users.noreply.github.com>
2025-11-14 18:27:55 +0000

    finishing the UI

--- app.py
+++ app.py
@@ -2,8 +2,13 @@ import streamlit as st
 import pandas as pd
 from joblib import load
 import numpy as np
+import re
 
-##model = load(xgboost.joblib)
+
+
+
+model = load("boost.joblib")
+
 df = pd.read_csv("cleaned_hotel_data.csv") 
 df = df.drop("is_canceled", axis=1)
 
@@ -11,11 +16,6 @@ st.title("Hotel Bookin Cancellation Predictor")
 
 columns =list(df)
 input= {}
-
-import numpy as np
-import streamlit as st
-
-input_data = {}
 
 for x in columns:
     # Determine if column is numeric
@@ -32,15 +32,27 @@ for x in columns:
     if val:
         if is_numeric:
             try:
-                input_data[x] = float(val)
+                input[x] = float(val)
             except ValueError:
                 st.error(f"❌ {x.replace("_", " ").title()} must be a number.")
         else:
             # Expect text, but reject pure numbers
-            if val:
-                input_data[x] = val
+            if re.fullmatch(r"[A-Za-zÀ-ÿ\s]+", val):
+                input[x] = val
                 
             else:
-                st.error(f"❌ {val} must be text, not a number.")    
+                st.error(f"❌ {x.replace("_", " ").title()} must be text.") 
 
-##check when the input must be text but it isn't
+with st.form(key='my_form_to_submit'):
+
+    predict_button = st.form_submit_button(label='Predict')
+
+if predict_button:
+
+    input_df = pd.DataFrame([input])
+    input_df = pd.get_dummies(input_df)
+    input_df = input_df.reindex(columns=model.get_booster().feature_names, fill_value=0)   
+    prediction = model.predict(input_df)[0]
+    probs = model.predict_proba(input_df)[0]
+    result = "❌ Canceled" if prediction == 1 else "✅ Not Canceled"
+    st.subheader(f"Prediction: {result} with a probabilty of {probs[prediction]*100}%")
